05c25dfb99e69af2144cc6a4617656efa494cc15
Mostafa <mostafafinal55@gmail.com>
2024-06-03 01:03:44 +0300

    Creating Random Backgrounds For The Landing Page

---
+++ js/js/master.js
@@ -0,0 +1,17 @@
+// Select Landing Page
+let lanadingPage = document.querySelector(".landing-page");
+
+// Landing Page Imgs
+let landingImgs = [
+  "landing-one.jpg",
+  "landing-two.jpg",
+  "landing-three.jpg",
+  "landing-four.jpg",
+  "landing-five.jpg",
+];
+
+// Random Imgs
+setInterval(() => {
+  let ranNum = Math.floor(Math.random() * landingImgs.length);
+  lanadingPage.style.backgroundImage = `url("img/${landingImgs[ranNum]}")`;
+}, 5000);
